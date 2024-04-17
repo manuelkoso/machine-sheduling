9d7db21dad4b3091d2418a518d80976610e06757
Manuel Kosovel <kosovelmanuel@gmail.com>
2024-04-17 23:11:09 +0200

    Update

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from src.ModelEvaluator import ModelEvaluator
 from src.MachineScheduler import MILPAdvanced
 from src.InstanceMeta import SyntheticInstanceMeta
+from src.ResultsAnalyzer import ResultsAnalyzer
 
 import os
 import logging
 
-OUTPUT_PATH = "results/results_milp_advanced.csv"
+OUTPUT_PATH = "output/results_milp_advanced.csv"
 
 
 def main():
     logging.basicConfig(filename="log\\log", level=logging.DEBUG,
                         format="%(asctime)s %(levelname)s %(module)s %(name)s %(message)s")
     logging.debug("Start main execution")
-    evaluator = ModelEvaluator(MILPAdvanced, OUTPUT_PATH)
-    evaluator.evaluate_all_instances(SyntheticInstanceMeta)
+    # evaluator = ModelEvaluator(MILPAdvanced, OUTPUT_PATH)
+    # evaluator.evaluate_all_instances(SyntheticInstanceMeta)
+    J_M_K = (50, 5, 3)
+    analyzer = ResultsAnalyzer()
+    print(analyzer.get_vars_mean(J_M_K))
+    print(analyzer.get_LP_mean(J_M_K))
+    print(analyzer.get_nzs_mean(J_M_K))
+    print(analyzer.get_constrs_mean(J_M_K))
 
 
 if __name__ == '__main__':
--- src/ResultsAnalyzer.py
+++ src/ResultsAnalyzer.py
@@ -14,43 +14,51 @@ class ResultsAnalyzer:
 
     def get_vars_mean(self, J_M_K: Tuple[int, int, int]) -> float:
         J, M, K = J_M_K
-        results = self.data_milp.loc[
-            (self.data_milp["number_of_jobs"] == J) & (self.data_milp["number_of_machines"] == M) & (self.data_milp[
-                                                                                                         "number_of_workers"] == K)]
+        optimum_versions = self.__get_optimum_instances(J_M_K)
+        results = self.data_milp.loc[(self.data_milp["version"].isin(optimum_versions["version"])) &
+                                     (self.data_milp["number_of_jobs"] == J) & (
+                                             self.data_milp["number_of_machines"] == M) & (self.data_milp[
+                                                                                               "number_of_workers"] == K)]
         return mean(results["number_of_variables"])
 
     def __get_optimum_instances(self, J_M_K: Tuple[int, int, int]) -> pd.DataFrame:
         J, M, K = J_M_K
         milp_versions = self.data_milp.loc[
             (self.data_milp["optimum_reached"] == True) & (self.data_milp["number_of_jobs"] == J) & (
-                        self.data_milp["number_of_machines"] == M) & (self.data_milp[
-                                                                          "number_of_workers"] == K)]["version"]
+                    self.data_milp["number_of_machines"] == M) & (self.data_milp[
+                                                                      "number_of_workers"] == K)]["version"]
         milp_advanced_versions = self.data_milp.loc[
             (self.data_milp["optimum_reached"] == True) & (self.data_milp["number_of_jobs"] == J) & (
                     self.data_milp["number_of_machines"] == M) & (self.data_milp[
-                                                     "number_of_workers"] == K)]["version"]
+                                                                      "number_of_workers"] == K)]["version"]
         versions = pd.merge(milp_versions, milp_advanced_versions)
         return versions
 
     def get_constrs_mean(self, J_M_K: Tuple[int, int, int]):
         J, M, K = J_M_K
-        results = self.data_milp.loc[
-            (self.data_milp["number_of_jobs"] == J) & (self.data_milp["number_of_machines"] == M) & (self.data_milp[
-                                                                                                         "number_of_workers"] == K)]
+        optimum_versions = self.__get_optimum_instances(J_M_K)
+        results = self.data_milp.loc[(self.data_milp["version"].isin(optimum_versions["version"])) &
+                                     (self.data_milp["number_of_jobs"] == J) & (
+                                                 self.data_milp["number_of_machines"] == M) & (self.data_milp[
+                                                                                                   "number_of_workers"] == K)]
         return mean(results["number_of_constraints"])
 
     def get_nzs_mean(self, J_M_K: Tuple[int, int, int]):
         J, M, K = J_M_K
-        results = self.data_milp.loc[
-            (self.data_milp["number_of_jobs"] == J) & (self.data_milp["number_of_machines"] == M) & (self.data_milp[
-                                                                                                         "number_of_workers"] == K)]
+        optimum_versions = self.__get_optimum_instances(J_M_K)
+        results = self.data_milp.loc[(self.data_milp["version"].isin(optimum_versions["version"])) &
+                                     (self.data_milp["number_of_jobs"] == J) & (
+                                                 self.data_milp["number_of_machines"] == M) & (self.data_milp[
+                                                                                                   "number_of_workers"] == K)]
         return mean(results["nzs"])
 
     def get_LP_mean(self, J_M_K: Tuple[int, int, int]):
         J, M, K = J_M_K
-        results = self.data_milp.loc[
-            (self.data_milp["number_of_jobs"] == J) & (self.data_milp["number_of_machines"] == M) & (self.data_milp[
-                                                                                                         "number_of_workers"] == K)]
+        optimum_versions = self.__get_optimum_instances(J_M_K)
+        results = self.data_milp.loc[(self.data_milp["version"].isin(optimum_versions["version"])) &
+                                     (self.data_milp["number_of_jobs"] == J) & (
+                                                 self.data_milp["number_of_machines"] == M) & (self.data_milp[
+                                                                                                   "number_of_workers"] == K)]
         return mean(results["LP"])
 
     def get_opt_count(self, p_q: bool = None, r: float = None, J: int = None, J_M_ratio: int = None,
